16d361d1a2181ea47a3df311dabe896948f10e3d
Daniel Sposito Coelho <spositocoelho@gmail.com>
2022-02-02 22:49:02 -0300

    há um bug no desenho

--- js/mecanicaJogo.js
+++ js/mecanicaJogo.js
@@ -1,6 +1,6 @@
 /*JOGO DA FORCA
 
- Deve funcionar só com letras maiúsculas;
+ Deve funcionar só com letras maiúsculas (use função para capitalizar todas por padrão);
 - Não devem ser utilizadas letras com acentos nem caracteres especiais;
 - Ao completar o desenho da forca, deve ser exibida uma mensagem na tela de "Fim de Jogo";
 - Se completar a palavra correta antes de acabarem as tentativas, deve ser exibida na tela a mensagem "Você Venceu. Parabéns!";
@@ -12,4 +12,111 @@
 
 Extras:
 - A página deve conter um campo para inserção de texto com a finalidade de adicionar novas palavras ao jogo, e um botão "Adicionar nova palavra"
+
+
+1. sortear palavra
+2. desenhar traços da palavra
+3. ler tecla digitada
+
+4. validar tecla: filtros e verifica se jogo acabou
+5a - se Ganhou: exibe mensagem de vitória
+! - pergunta se quer outra palavra
+
+5b - se Perdeu: exibe mensagem de derrota
+!
+
+5. - se continua: verificar jogada
+6A SE ACERTO - adiciona letra sobre o traço correto 
+
+6B SE ERRO - verifica se letra já foi errada antes
+7B - caso não, adiciona letra errada a uma lista de erros ao lado, sim não adiciona
+7B - verifica estado atual do desenho 
+7B - adiciona próximo traço ao desenho
+
 */
+var inputNewWord = document.querySelector('#newWord');
+var btnAddNewWord = document.querySelector('#addWord');
+var startGame = document.querySelector('#begin');
+var secretWords = ['ABACAXI','CADERNO','GARRAFA','MOCHILA','ACADEMIA','GUITARRA','TATUAGEM','CARRO','GASOLINA','CELULAR','COMPUTADOR',
+                    'GADO','ARO','PILULA','OUVIDOR','ALFAIATE','EQUILIBRISTA','EXTENSOR']
+var gameArea = document.querySelector('#gameArea');
+var giz = gameArea.getContext('2d');
+btnAddNewWord.addEventListener('click', function addNewWord(){
+
+})
+function validateNewWord ()
+{
+
+}                    
+
+startGame.addEventListener('click', function newGame (){
+    // checa se o jogo acabou ?
+    // reset
+    // desenhando campo do jogo               
+    var word = genRandomWord(); // gerando palavra secreta aleatória
+    drawWordGaps(word);
+})   
+
+
+function genRandomWord ()
+{
+    var randomIndex = Math.floor(Math.random()*(secretWords.length-1));
+    return secretWords[randomIndex];
+}
+
+function drawWordGaps (word)
+{
+    var y = 600;
+    var x = 200;
+    var lineSize = 60;
+    var gap = 20;
+    giz.strokeStyle='white';
+
+    for(var i = 0; i<word.length; i++)
+    {
+        giz.beginPath(); //começa um caminho, outra forma dentro desse contexto desse canvas
+        giz.moveTo(x,y);//estabelece o ponto de início
+        giz.lineTo(x+lineSize,y);//faz linha
+        giz.closePath();
+        giz.stroke();//preenche o último path iniciado
+        x += (lineSize+gap);
+    }
+	
+}
+
+function validateKey ()
+{
+
+}
+
+function checkGuess ()
+{
+
+}
+
+function scoresPoints ()
+{
+
+}
+
+function drawHangman ()
+{
+
+}
+
+function isEnd ()
+{
+
+}
+
+function popWinMsg ()
+{
+
+}
+
+function popLossMsg ()
+{
+
+}
+
+
